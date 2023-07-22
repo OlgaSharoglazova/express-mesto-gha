e51289b6199db41298fc1cab23092d160da5c0c8
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-07-22 15:23:52 +0800

    роуты лайков

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const cardsRoutes = require('express').Router();
-const { getCards, deleteCard, createCard } = require('../controllers/cards');
+const {
+  getCards,
+  deleteCard,
+  createCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
 
 cardsRoutes.get('/', getCards);
 
 cardsRoutes.post('/', express.json(), createCard);
 
 cardsRoutes.delete('/:cardId', deleteCard);
+
+cardsRoutes.put('/:cardId/likes', likeCard);
+
+cardsRoutes.delete('/:cardId/likes', dislikeCard);
 
 module.exports = cardsRoutes;
