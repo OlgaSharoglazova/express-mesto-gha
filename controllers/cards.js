8057bfe4480f744e22281923005c8b51234aa30b
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-07-25 00:15:42 +0800

    обработка ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,21 +1,27 @@
 const Card = require('../models/card');
+const {
+  SUCCESS,
+  BAD_REQUEST,
+  NOT_FOUND,
+  DEFAULT_ERROR,
+} = require('../utils/constants');
 
 module.exports.getCards = (_req, res) => {
   Card.find({})
-    .then((cards) => res.status(200).send(cards))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .then((cards) => res.status(SUCCESS).send(cards))
+    .catch(() => res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send(card))
+    .then((card) => res.status(SUCCESS).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
@@ -23,14 +29,14 @@ module.exports.deleteCard = (req, res) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: 'Нет карточки с таким id' });
+        res.status(NOT_FOUND).send({ message: 'Нет карточки с таким id' });
       }
       Card.findByIdAndRemove(req.params.cardId);
     })
     .then(() => {
-      res.status(200).send({ message: 'Карточка удалена' });
+      res.status(SUCCESS).send({ message: 'Карточка удалена' });
     })
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .catch(() => res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
@@ -38,13 +44,27 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
   { new: true },
 )
-  .then((card) => res.send(card))
-  .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+  .then((card) => {
+    if (!card) {
+      res.status(NOT_FOUND).send({ message: 'Нет карточки с таким id' });
+    } res.status(SUCCESS).send(card);
+  })
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+    }
+    res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+  });
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } }, // убрать _id из массива
   { new: true },
 )
-  .then((card) => res.send(card))
-  .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+  .then((card) => res.status(SUCCESS).send(card))
+  .catch((err) => {
+    if (err.name === 'ValidationError') {
+      res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+    }
+    res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+  });
--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,14 @@
 const User = require('../models/user');
+const {
+  SUCCESS,
+  BAD_REQUEST,
+  NOT_FOUND,
+  DEFAULT_ERROR,
+} = require('../utils/constants');
 
 module.exports.getUsers = (_req, res) => {
   User.find({})
-    .then((users) => res.status(200).send({ data: users }))
+    .then((users) => res.status(SUCCESS).send({ data: users }))
     .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
@@ -10,23 +16,23 @@ module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: 'Нет пользователя с таким id' });
+        res.status(NOT_FOUND).send({ message: 'Нет пользователя с таким id' });
       }
-      res.status(200).send(user);
+      res.status(SUCCESS).send(user);
     })
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .catch(() => res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' }));
 };
 
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.status(200).send(user))
+    .then((user) => res.status(SUCCESS).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
-      res.status(500).send({ message: 'На сервере произошла ошибка' });
+      res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
     });
 };
 
@@ -34,14 +40,24 @@ module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((newData) => res.send(newData))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .then((newData) => res.status(SUCCESS).send(newData))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((newAvatar) => res.send(newAvatar))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .then((newAvatar) => res.status(SUCCESS).send(newAvatar))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
+    });
 };
