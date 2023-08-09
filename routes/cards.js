19a12044e381c336b1f9a11ce258fd3e4c1bb27e
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-09 14:31:44 +0800

    регулярное выражение

--- routes/cards.js
+++ routes/cards.js
@@ -15,7 +15,7 @@ cardsRoutes.get('/', getCards);
 cardsRoutes.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().regex(regularExpression),
+    link: Joi.string().required().pattern(regularExpression),
   }),
 }), createCard);
 
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ routes.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(regularExpression),
+    avatar: Joi.string().pattern(regularExpression),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
