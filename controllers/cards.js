e386ba36a1fc0283dc2085ff0a928c9f5da6744c
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-09 16:41:17 +0800

    удаление

--- controllers/cards.js
+++ controllers/cards.js
@@ -28,9 +28,9 @@ module.exports.deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (card.owner !== req.user._id) {
-        next(new Forbidden('Нельзя удалить чужую карточку'));
-      } else if (!card) {
-        next(new NotFound('Нет карточки с таким id'));
+        throw new Forbidden('Нельзя удалить чужую карточку');
+      } if (!card) {
+        throw new NotFound('Нет карточки с таким id');
       }
       return Card.findByIdAndRemove(req.params.cardId);
     })
