0f3c9ee8f6aa046c5bedcd98f0ef3b3d95bbce7f
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-08-10 13:16:19 +0800

    del card 15

--- controllers/cards.js
+++ controllers/cards.js
@@ -37,7 +37,7 @@ module.exports.deleteCard = (req, res, next) => {
     .then((card) => {
       Card.deleteOne(card)
         .then(() => {
-          res.send('Нельзя удалять карточки других пользователей');
+          res.send(card);
         });
     })
     .catch((err) => {
